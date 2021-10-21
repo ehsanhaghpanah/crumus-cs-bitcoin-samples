495dad6a2901bcf344793f1f0e4301abc6e9c84b
Ehsan Haghpanah <anonim.developer@gmail.com>
2021-10-21 12:42:21 +0330

    .

--- Sample1/Program.cs
+++ Sample1/Program.cs
@@ -1,15 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/**
+ * Copyright (C) crumüs, 2018.
+ * All rights reserved.
+ * Ehsan Haghpanah; haghpanah@crumus.com
+ */
+
+using System;
+using NBitcoin;
 
 namespace Sample1
 {
-	class Program
+	static public class Program
 	{
 		static void Main(string[] args)
 		{
+			var ky = new Key().GetWif(Network.Main);
+			Console.WriteLine($"key = {ky}");
 		}
 	}
 }
